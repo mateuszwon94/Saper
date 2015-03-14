73429dfa76c8ab73f2a935d3eedeea738ce4e405
mateuszwon94 <mateusz.winiarski.94@gmail.com>
2015-03-14 23:40:47 +0100

    Okno biale :P

--- Saper/Saper.cpp
+++ Saper/Saper.cpp
@@ -8,10 +8,37 @@ int main(int argc, char *argv[]) {
 		printf("Your terminal does not support color\n");
 		return 1;
 	}
+
+	curs_set(FALSE);
+
 	start_color();			/* Start color 			*/
 	assume_default_colors(COLOR_WHITE, COLOR_BLUE);
-	printw("Jakis napis");
+
+	init_pair(1, COLOR_BLACK, COLOR_WHITE);
+	init_pair(2, COLOR_WHITE, COLOR_BLACK);
+
+	WINDOW* box = newwin(LINES - 5, COLS - 8, 3, 4);
+	WINDOW* shadow = newwin(1, 1, 0, 0);
+
+	wattron(box, COLOR_PAIR(1));
+	//wattron(shadow, COLOR_PAIR(2));
+	wbkgd(shadow, COLOR_PAIR(2));
+	wrefresh(shadow);
+	
+	printw("Saper v0.0.0\n__________________________________________________________________________________________________________________________________________________________________________");
 	refresh();
+
+	wattron(box, COLOR_PAIR(1));
+	wbkgd(box, COLOR_PAIR(1));
+	mvwprintw(box, 5, 20, "tekst");
+	wrefresh(box);
+	
+	wattroff(box, COLOR_PAIR(1));
+	//wattroff(shadow, COLOR_PAIR(2));
+
+	delwin(box);
+	delwin(shadow);
+
 	getch();
 	endwin();
 
