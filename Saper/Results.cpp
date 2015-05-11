c13f909b998e8d84f4691d79739f03146bd7ead4
Magda <magdaalidia@gmail.com>
2015-05-11 13:46:26 +0200

    zalazki wynikow, komentarze nie usuwaj sa jako przypomnienie gdzi i co

--- Saper/Results.cpp
+++ Saper/Results.cpp
@@ -24,3 +24,34 @@ Results& Results::operator<<(string text) {
 	file << text;
 	return *(this);
 }
+void Results::load() {
+	string text;
+	while (getline(file, text))
+	{
+		int first = 0;
+		if (text[0] == 'l') {
+			first = text.find_first_of(' ');
+			int czas = copy(first, text);
+			easy.push_back(czas);
+		}
+		else if (text[0] == 's') {
+			first = text.find_first_of(' ');
+			int czas = copy(first, text);
+			mild.push_back(czas);
+		}
+		else if (text[0] == 't') {
+			first = text.find_first_of(' ');
+			int czas = copy(first, text);
+			hard.push_back(czas);
+		}
+		else {
+			//PRZEMYŒLE Z ROZMIARAMI PLANSZY!!!
+		}
+	}
+}
+int Results::copy(int first, string text) {
+	char czas[4];
+	std::size_t length = text.copy(czas, 4, first);
+	czas[length] = '\0';
+	int i_czas = atoi(czas);
+}
--- Saper/Results.h
+++ Saper/Results.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <fstream>
 #include "Window.h"
+#include <vector>
 class Results {
 public:
 	Results();
@@ -10,8 +11,15 @@ public:
 	static Results* getInstance();
 	bool getProblem(){ return problem; }
 	Results& operator<<(std::string text);
+	void load();
 private:
+	int copy(int first, std::string text);
+	std::vector<int> easy;
+	std::vector<int> mild;
+	std::vector<int> hard;
+	std::vector<int> own;
 	std::fstream file;
 	static Results* _instance;
 	bool problem;
+	//SORTOWANIE
 };
